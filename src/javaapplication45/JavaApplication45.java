79712625cacdda5a9238d05309241308a70040d2
Vanderlei K Silva <vanderleis47@gmail.com>
2021-09-09 20:33:09 -0300

    Add item lista

---
+++ src/javaapplication45/JavaApplication45.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication45;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author vande
+ */
+public class JavaApplication45 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        List<Integer> lista = new ArrayList<>();
+        
+        lista.add(29);
+        lista.add(30);
+        lista.add(35);
+        lista.add(58);
+        lista.add(45);
+        System.out.println("ola");
+        lista.forEach(i -> {
+            System.out.println(i);
+        });
+        
+        
+    }
+}
